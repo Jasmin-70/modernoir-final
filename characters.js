daaabbaccf749ce2f6d00777c0bc8cd373d0faa4
Jasmin-70 <jmp658@scarletmail.rutgers.edu>
2025-12-18 16:30:29 -0500

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,277 @@
+
+function playAudio(audioId) {
+    var audio = document.getElementById(audioId);
+    if (audio) {
+        audio.currentTime = 0; // Reset audio to the beginning
+        audio.play();
+    }
+}
+
+// Function to pause audio (optional, but good for short effects)
+function pauseAudio(audioId) {
+    var audio = document.getElementById(audioId);
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0; // Reset after pause
+    }
+}
+
+document.getElementById('hoverDiv').addEventListener('mouseover', function() {
+    playAudio('hoverAudio');
+});
+document.getElementById('hoverDiv').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio');
+});
+
+document.getElementById('hoverDiv1').addEventListener('mouseover', function() {
+    playAudio('hoverAudio1');
+});
+document.getElementById('hoverDiv1').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio1');
+});
+
+document.getElementById('hoverDiv2').addEventListener('mouseover', function() {
+    playAudio('hoverAudio2');
+});
+document.getElementById('hoverDiv2').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio2');
+});
+
+document.getElementById('hoverDiv3').addEventListener('mouseover', function() {
+    playAudio('hoverAudio3');
+});
+document.getElementById('hoverDiv3').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio3');
+});
+
+document.getElementById('hoverDiv4').addEventListener('mouseover', function() {
+    playAudio('hoverAudio4');
+});
+document.getElementById('hoverDiv4').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio4');
+});
+
+document.getElementById('hoverDiv5').addEventListener('mouseover', function() {
+    playAudio('hoverAudio5');
+});
+document.getElementById('hoverDiv5').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio5');
+});
+
+document.getElementById('hoverDiv6').addEventListener('mouseover', function() {
+    playAudio('hoverAudio6');
+});
+document.getElementById('hoverDiv6').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio6');
+});
+
+document.getElementById('hoverDiv7').addEventListener('mouseover', function() {
+    playAudio('hoverAudio7');
+});
+document.getElementById('hoverDiv7').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio7');
+});
+
+document.getElementById('hoverDiv8').addEventListener('mouseover', function() {
+    playAudio('hoverAudio8');
+});
+document.getElementById('hoverDiv8').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio8');
+});
+
+document.getElementById('hoverDiv9').addEventListener('mouseover', function() {
+    playAudio('hoverAudio9');
+});
+document.getElementById('hoverDiv9').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio9');
+});
+
+document.getElementById('hoverDiv10').addEventListener('mouseover', function() {
+    playAudio('hoverAudio10');
+});
+document.getElementById('hoverDiv10').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio10');
+});
+
+document.getElementById('hoverDiv11').addEventListener('mouseover', function() {
+    playAudio('hoverAudio11');
+});
+document.getElementById('hoverDiv11').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio11');
+});
+
+document.getElementById('hoverDiv12').addEventListener('mouseover', function() {
+    playAudio('hoverAudio12');
+});
+document.getElementById('hoverDiv12').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio12');
+});
+
+document.getElementById('hoverDiv13').addEventListener('mouseover', function() {
+    playAudio('hoverAudio13');
+});
+document.getElementById('hoverDiv13').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio13');
+});
+
+document.getElementById('hoverDiv14').addEventListener('mouseover', function() {
+    playAudio('hoverAudio14');
+});
+document.getElementById('hoverDiv14').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio14');
+});
+
+document.getElementById('hoverDiv15').addEventListener('mouseover', function() {
+    playAudio('hoverAudio15');
+});
+document.getElementById('hoverDiv15').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio15');
+});
+
+document.getElementById('hoverDiv16').addEventListener('mouseover', function() {
+    playAudio('hoverAudio16');
+});
+document.getElementById('hoverDiv16').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio16');
+});
+
+document.getElementById('hoverDiv17').addEventListener('mouseover', function() {
+    playAudio('hoverAudio17');
+});
+document.getElementById('hoverDiv17').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio17');
+});
+
+document.getElementById('hoverDiv18').addEventListener('mouseover', function() {
+    playAudio('hoverAudio18');
+});
+document.getElementById('hoverDiv18').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio18');
+});
+
+document.getElementById('hoverDiv19').addEventListener('mouseover', function() {
+    playAudio('hoverAudio19');
+});
+document.getElementById('hoverDiv19').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio19');
+});
+
+document.getElementById('hoverDiv20').addEventListener('mouseover', function() {
+    playAudio('hoverAudio20');
+});
+document.getElementById('hoverDiv20').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio20');
+});
+
+document.getElementById('hoverDiv21').addEventListener('mouseover', function() {
+    playAudio('hoverAudio21');
+});
+document.getElementById('hoverDiv21').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio21');
+});
+
+document.getElementById('hoverDiv22').addEventListener('mouseover', function() {
+    playAudio('hoverAudio22');
+});
+document.getElementById('hoverDiv22').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio22');
+});
+
+document.getElementById('hoverDiv23').addEventListener('mouseover', function() {
+    playAudio('hoverAudio23');
+});
+document.getElementById('hoverDiv23').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio23');
+});
+
+document.getElementById('hoverDiv24').addEventListener('mouseover', function() {
+    playAudio('hoverAudio24');
+});
+document.getElementById('hoverDiv24').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio24');
+});
+
+document.getElementById('hoverDiv25').addEventListener('mouseover', function() {
+    playAudio('hoverAudio25');
+});
+document.getElementById('hoverDiv25').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio25');
+});
+
+document.getElementById('hoverDiv26').addEventListener('mouseover', function() {
+    playAudio('hoverAudio26');
+});
+document.getElementById('hoverDiv26').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio26');
+});
+
+document.getElementById('hoverDiv27').addEventListener('mouseover', function() {
+    playAudio('hoverAudio27');
+});
+document.getElementById('hoverDiv27').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio27');
+});
+
+document.getElementById('hoverDiv28').addEventListener('mouseover', function() {
+    playAudio('hoverAudio28');
+});
+document.getElementById('hoverDiv28').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio28');
+});
+
+document.getElementById('hoverDiv29').addEventListener('mouseover', function() {
+    playAudio('hoverAudio29');
+});
+document.getElementById('hoverDiv29').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio29');
+});
+
+document.getElementById('hoverDiv30').addEventListener('mouseover', function() {
+    playAudio('hoverAudio30');
+});
+document.getElementById('hoverDiv30').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio30');
+});
+
+document.getElementById('hoverDiv31').addEventListener('mouseover', function() {
+    playAudio('hoverAudio31');
+});
+document.getElementById('hoverDiv31').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio31');
+});
+
+document.getElementById('hoverDiv32').addEventListener('mouseover', function() {
+    playAudio('hoverAudio32');
+});
+document.getElementById('hoverDiv32').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio32');
+});
+
+document.getElementById('hoverDiv33').addEventListener('mouseover', function() {
+    playAudio('hoverAudio33');
+});
+document.getElementById('hoverDiv33').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio33');
+});
+
+document.getElementById('hoverDiv34').addEventListener('mouseover', function() {
+    playAudio('hoverAudio34');
+});
+document.getElementById('hoverDiv34').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio34');
+});
+
+document.getElementById('hoverDiv35').addEventListener('mouseover', function() {
+    playAudio('hoverAudio35');
+});
+document.getElementById('hoverDiv35').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio35');
+});
+
+document.getElementById('hoverDiv36').addEventListener('mouseover', function() {
+    playAudio('hoverAudio36');
+});
+document.getElementById('hoverDiv36').addEventListener('mouseout', function() {
+    pauseAudio('hoverAudio36');
+});
+
